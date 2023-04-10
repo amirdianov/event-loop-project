3ecd2799891ba072ee8a2001dacdece31f8bd17d
diyanovamir <amirdianov@gmail.com>
2023-04-11 00:07:04 +0300

    + user serializer, viewset

--- backend/authorisation_token/serializers.py
+++ backend/authorisation_token/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
+from authorisation_token.models import User
+
 
 class StatusCheckSerializer(serializers.Serializer):
     status = serializers.CharField()
     user_id = serializers.IntegerField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
--- backend/authorisation_token/views.py
+++ backend/authorisation_token/views.py
@@ -2,14 +2,21 @@ import time
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from authorisation_token.serializers import StatusCheckSerializer
+from authorisation_token.models import User
+from authorisation_token.serializers import StatusCheckSerializer, UserSerializer
 
 
 # Create your views here.
 @api_view()
 def status_view(request):
-    time.sleep(3)
+    # time.sleep(3)
     return Response(
         StatusCheckSerializer({"status": "ok", "user_id": request.user.id}).data
     )
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.object.all()
+    serializer_class = UserSerializer
