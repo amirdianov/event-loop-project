b0ab27c284f18eb7b0970095f28cfde68ad09b8b
diyanovamir <amirdianov@gmail.com>
2023-04-11 23:59:35 +0300

    + store

--- frontend/event-loop/src/main.js
+++ frontend/event-loop/src/main.js
@@ -3,6 +3,7 @@ import {createPinia} from 'pinia'
 import Antd from 'ant-design-vue';
 
 import App from './App.vue'
+import store from './store/index'
 import router from './router'
 import 'ant-design-vue/dist/antd.css';
 
@@ -12,4 +13,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(Antd)
+app.use(store)
 app.mount('#app')
