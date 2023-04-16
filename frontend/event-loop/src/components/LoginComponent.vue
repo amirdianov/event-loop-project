76fe9d1766d3b89a30233780b6a2e1b75eb79dc5
diyanovamir <amirdianov@gmail.com>
2023-04-17 00:53:25 +0300

    + some icons

--- frontend/event-loop/src/components/LoginComponent.vue
+++ frontend/event-loop/src/components/LoginComponent.vue
@@ -13,7 +13,11 @@
                 name="username"
                 :rules="[{ required: true, message: 'Please input your username!' }]"
         >
-            <a-input v-model:value="formState.username"/>
+            <a-input v-model:value="formState.username">
+                <template #prefix>
+                    <MailOutlined class="site-form-item-icon"/>
+                </template>
+            </a-input>
         </a-form-item>
 
         <a-form-item
@@ -21,7 +25,11 @@
                 name="password"
                 :rules="[{ required: true, message: 'Please input your password!' }]"
         >
-            <a-input-password v-model:value="formState.password"/>
+            <a-input-password v-model:value="formState.password">
+                <template #prefix>
+                    <LockOutlined class="site-form-item-icon"/>
+                </template>
+            </a-input-password>
         </a-form-item>
 
         <a-form-item name="remember" :wrapper-col="{ offset: 5, span: 16 }">
@@ -29,18 +37,19 @@
         </a-form-item>
 
         <a-form-item :wrapper-col="{ offset: 5, span: 16 }">
-            <a-button type="primary" html-type="submit" style="margin-right: 10px">Войти</a-button>
-            <a-button type="primary" @click="$router.push('/registration')">Регистрация</a-button>
-
+            <a-button type="primary" html-type="submit" style="margin-right: 10px; width: 100%">Войти</a-button>
+            <router-link to="/registration">Регистрация</router-link>
         </a-form-item>
     </a-form>
 </template>
 <script>
 import {defineComponent, reactive} from 'vue';
 import {mapActions, mapMutations, mapState} from "vuex";
+import {LockOutlined, UserOutlined, MailOutlined} from "@ant-design/icons-vue";
 
 export default defineComponent({
     name: "LoginComponent",
+    components: {LockOutlined, MailOutlined},
     setup() {
         const formState = reactive({
             username: '',
