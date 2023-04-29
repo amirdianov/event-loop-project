047f5a8b95b8bbb6dcd88e5840bad042468ea984
diyanovamir <amirdianov@gmail.com>
2023-04-29 17:03:11 +0300

    + create component

---
+++ frontend/event-loop/src/containers/CreateEditEventInformationComponent.vue
@@ -0,0 +1,140 @@
+<template>
+    <a-form
+            :model="formState"
+            v-bind="layout"
+            name="nest-messages"
+            :validate-messages="validateMessages"
+            @finish="sub"
+            :wrapper-col="{span: 21}"
+            :label-col="{span: 3}"
+    >
+        <a-form-item :name="['event', 'title']" label="Title">
+            <a-input v-model:value="formState.event.title"/>
+        </a-form-item>
+        <a-form-item :name="['event', 'category']" label="Category">
+            <a-select
+                    ref="select"
+                    v-model:value="formState.event.category"
+                    :options="options1"
+            ></a-select>
+        </a-form-item>
+        <a-form-item :name="['event', 'start_time']" label="Start Time">
+            <time-component @update:model-value="formState.event.start_time = $event"></time-component>
+        </a-form-item>
+        <a-form-item :name="['event', 'finish_time']" label="Finish Time">
+            <time-component @update:model-value="formState.event.finish_time = $event"></time-component>
+        </a-form-item>
+
+        <a-form-item :name="['event', 'description']" label="Description">
+            <a-textarea v-model:value="formState.event.description"/>
+        </a-form-item>
+        <!--        TODO antdv upload file-->
+        <a-form-item :name="['event', 'photo']" label="Photo">
+            <!--                        <upload-component v-model:value="formState.event.photo"></upload-component>-->
+            <input type="file" ref="file">
+
+        </a-form-item>
+        <a-form-item :wrapper-col="{ ...layout.wrapperCol, offset: 3}">
+            <a-button type="primary" html-type="submit">Submit</a-button>
+        </a-form-item>
+    </a-form>
+</template>
+<script>
+import {defineComponent, reactive, ref} from 'vue';
+import UploadComponent from "@/components/UploadComponent.vue";
+import TimeComponent from "@/components/TimeComponent.vue";
+import {mapActions} from "vuex";
+
+export default defineComponent({
+    name: 'CreateEditEventInformationComponent',
+    components: {TimeComponent, UploadComponent},
+    data() {
+        return {
+            val: ''
+
+        }
+    },
+    setup() {
+        const layout = {
+            labelCol: {
+                span: 8,
+            },
+            wrapperCol: {
+                span: 16,
+            },
+        };
+        const validateMessages = {
+            required: '${label} is required!',
+            types: {
+                email: '${label} is not a valid email!',
+                number: '${label} is not a valid number!',
+            },
+            number: {
+                range: '${label} must be between ${min} and ${max}',
+            },
+        };
+        const options1 = ref([{
+            value: 'lesson',
+            label: 'Урок',
+        }, {
+            value: 'webinar',
+            label: 'Вебинар',
+        }, {
+            value: 'masterclass',
+            label: 'Мастер-класс',
+        }, {
+            value: 'entertainments',
+            label: 'Развлечения',
+        }]);
+        const formState = reactive({
+            event: {
+                title: '',
+                description: undefined,
+                start_time: '',
+                finish_time: '',
+                photo: '',
+                category: '',
+            },
+        });
+        const onFinish = values => {
+            console.log(values)
+        };
+        return {
+            formState,
+            onFinish,
+            layout,
+            validateMessages,
+            options1
+        };
+    },
+    methods: {
+        ...mapActions({createUsersEvent: 'events/createUsersEvent'}),
+        async sub(data) {
+            const formData = new FormData();
+            let keys = Object.keys(data['event'])
+            keys.forEach((key) => {
+                console.log(key, data['event'][key])
+                formData.append(key, data['event'][key])
+                console.log(formData.get(key))
+            })
+            formData.append('photo', this.$refs.file.files[0]);
+            try {
+                await this.createUsersEvent(formData)
+                this.$router.push({name: 'my-events'})
+
+            } catch (e) {
+                console.log(e)
+            }
+        },
+    }
+    //     async load() {
+    //         this.isLoading = true;
+    //         this.results = await getTags();
+    //         this.isLoading = false;
+    //     }
+    // },
+    // created() {
+    //     this.load();
+    // },
+});
+</script>
--- frontend/event-loop/src/views/user/CreateEditUsersEventView.vue
+++ frontend/event-loop/src/views/user/CreateEditUsersEventView.vue
@@ -1,107 +1,32 @@
 <template>
-    <a-form
-            :model="formState"
-            v-bind="layout"
-            name="nest-messages"
-            :validate-messages="validateMessages"
-            @finish="sub"
-    >
-        <a-form-item :name="['event', 'title']" label="Title" :rules="[{ required: true }]">
-            <a-input v-model:value="formState.event.title"/>
-        </a-form-item>
-        <a-form-item :name="['event', 'description']" label="Description" :rules="[{ required: true }]">
-            <a-input v-model:value="formState.event.description"/>
-        </a-form-item>
-        <a-form-item :name="['event', 'start_time']" label="Start Time">
-            <time-component @update:model-value="formState.event.start_time = $event"></time-component>
-        </a-form-item>
-        <a-form-item :name="['event', 'finish_time']" label="Finish Time">
-            <time-component @update:model-value="formState.event.finish_time = $event"></time-component>
-        </a-form-item>
-        <a-form-item :name="['event', 'category']" label="Category">
-            <a-textarea v-model:value="formState.event.category"/>
-        </a-form-item>
-<!--        TODO antdv upload file-->
-        <a-form-item :name="['event', 'photo']" label="Photo">
-<!--                        <upload-component v-model:value="formState.event.photo"></upload-component>-->
-            <input type="file" ref="file">
-
-        </a-form-item>
-        <a-form-item :wrapper-col="{ ...layout.wrapperCol, offset: 8 }">
-            <a-button type="primary" html-type="submit">Submit</a-button>
-        </a-form-item>
-    </a-form>
+<!--    <a-alert v-if="this.isSuccess"-->
+<!--             message="Успешно"-->
+<!--             description="Информация обновлена!"-->
+<!--             type="success"-->
+<!--             show-icon-->
+<!--    />-->
+    <a-alert v-if="this.error" :message="this.error" type="error"/>
+    <a-row type="flex" justify="space-around" align="middle" style="height: 100%">
+        <a-col :span="18" v-if="!isLoading">
+            <CreateEditEventInformationComponent></CreateEditEventInformationComponent>
+        </a-col>
+    </a-row>
 </template>
 <script>
-import {defineComponent, reactive} from 'vue';
-import UploadComponent from "@/components/UploadComponent.vue";
-import TimeComponent from "@/components/TimeComponent.vue";
-import {mapActions} from "vuex";
+import {defineComponent} from 'vue';
+import CreateEditEventInformationComponent from "@/containers/CreateEditEventInformationComponent.vue";
+import {mapState} from "vuex";
+
 
 export default defineComponent({
-    components: {TimeComponent, UploadComponent},
-    data() {
-        return{
-            val: ''
-        }
+    name: "CreateEditUsersEventView",
+    components: {CreateEditEventInformationComponent},
+    computed: {
+        ...mapState({
+            isLoading: state => state.login.isLoading,
+            error: state => state.login.error,
+        })
     },
-    setup() {
-        const layout = {
-            labelCol: {
-                span: 8,
-            },
-            wrapperCol: {
-                span: 16,
-            },
-        };
-        const validateMessages = {
-            required: '${label} is required!',
-            types: {
-                email: '${label} is not a valid email!',
-                number: '${label} is not a valid number!',
-            },
-            number: {
-                range: '${label} must be between ${min} and ${max}',
-            },
-        };
-        const formState = reactive({
-            event: {
-                title: '',
-                description: undefined,
-                start_time: '',
-                finish_time: '',
-                category: '',
-                photo: ''
-            },
-        });
-        const onFinish = values => {
-            console.log(values)
-        };
-        return {
-            formState,
-            onFinish,
-            layout,
-            validateMessages,
-        };
-    },
-    methods: {
-        ...mapActions({createUsersEvent: 'events/createUsersEvent'}),
-        async sub(data) {
-            const formData = new FormData();
-            let keys = Object.keys(data['event'])
-            keys.forEach((key) => {
-                console.log(key, data['event'][key])
-                formData.append(key, data['event'][key])
-                console.log(formData.get(key))
-            })
-            formData.append('photo', this.$refs.file.files[0]);
-            try {
-                await this.createUsersEvent(formData)
-            } catch (e) {
-                console.log(e)
-            }
-        }
 
-    },
 });
 </script>
