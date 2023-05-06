97198c3b66f48eab1289c58335a219bf172f3348
diyanovamir <amirdianov@gmail.com>
2023-05-06 13:53:46 +0300

    + validate reset password form in vue

--- frontend/event-loop/src/views/general/ResetPasswordView.vue
+++ frontend/event-loop/src/views/general/ResetPasswordView.vue
@@ -2,37 +2,27 @@
     <a-row type="flex" justify="center" align="middle" style="min-height: 550px">
         <a-col :span="10">
             <a-form
-                    :model="formState"
-                    name="basic"
                     :label-col="{ span: 6 }"
                     :wrapper-col="{ span: 15 }"
-                    :validate-messages="validateMessages"
-                    autocomplete="off"
+                    ref="formRef"
+                    name="custom-validation"
+                    :model="formState"
+                    :rules="rules"
                     @finish="submit"
-                    @finishFailed="onFinishFailed"
             >
-                <a-form-item
-                        label="Пароль"
-                        name="password"
-                        :rules="[{ required: true, type: 'password'}]"
-                >
-                    <a-input-password v-model:value="formState.password">
+                <a-form-item has-feedback label="Пароль" name="pass">
+                    <a-input v-model:value="formState.pass" type="password" autocomplete="off">
                         <template #prefix>
                             <LockOutlined class="site-form-item-icon"/>
                         </template>
-                    </a-input-password>
+                    </a-input>
                 </a-form-item>
-
-                <a-form-item
-                        label="Повторный пароль"
-                        name="password_confirm"
-                        :rules="[{ required: true}]"
-                >
-                    <a-input-password v-model:value="formState.password_confirm">
+                <a-form-item has-feedback label="Повторный пароль" name="checkPass">
+                    <a-input v-model:value="formState.checkPass" type="password" autocomplete="off">
                         <template #prefix>
                             <LockOutlined class="site-form-item-icon"/>
                         </template>
-                    </a-input-password>
+                    </a-input>
                 </a-form-item>
                 <a-form-item :wrapper-col="{ offset: 6, span: 15 }">
                     <a-button type="primary" html-type="submit" style="width: 100%">Изменить пароль</a-button>
@@ -44,7 +34,7 @@
 
 
 <script>
-import {defineComponent, reactive} from 'vue';
+import {defineComponent, reactive, ref} from 'vue';
 import {mapState} from "vuex";
 import {LockOutlined, MailOutlined} from "@ant-design/icons-vue";
 
@@ -52,27 +42,53 @@ export default defineComponent({
     name: "ResetPasswordView",
     components: {LockOutlined, MailOutlined},
     setup() {
+        const formRef = ref();
         const formState = reactive({
-            password: '',
-            password_confirm: '',
+            pass: '',
+            checkPass: '',
+            age: undefined,
         });
-        const onFinish = values => {
-            console.log('Success:', values);
+        let validatePass = async (_rule, value) => {
+            if (value === '') {
+                return Promise.reject('Введите пароль');
+            } else if (value.length < 5) {
+                return Promise.reject('Пароль должен быть содержать не менее 5 символов');
+            } else {
+                if (formState.checkPass !== '') {
+                    formRef.value.validateFields('checkPass');
+                }
+                return Promise.resolve();
+            }
         };
-        const onFinishFailed = errorInfo => {
-            console.log('Failed:', errorInfo);
+        let validatePass2 = async (_rule, value) => {
+            if (value === '') {
+                return Promise.reject('Повторите пароль');
+            } else if (value.length < 5) {
+                return Promise.reject('Пароль должен быть содержать не менее 5 символов');
+            } else if (value !== formState.pass) {
+                return Promise.reject("Пароли не совпадают");
+            } else {
+                return Promise.resolve();
+            }
         };
-        const validateMessages = {
-            required: 'Поле должно быть заполнено!',
-            types: {
-                email: '${label} неверно введена!',
-            },
+        const rules = {
+            pass: [{
+                required: true,
+                min: 10,
+                validator: validatePass,
+                trigger: 'change',
+            }],
+            checkPass: [{
+                required: true,
+                min: 10,
+                validator: validatePass2,
+                trigger: 'change',
+            }],
         };
         return {
             formState,
-            onFinish,
-            onFinishFailed,
-            validateMessages
+            formRef,
+            rules,
         };
     },
     computed: {
@@ -80,6 +96,11 @@ export default defineComponent({
             error: state => state.login.error
         })
     },
+    methods: {
+        submit(data) {
+            console.log(data)
+        }
+    }
 
 })
 </script>
