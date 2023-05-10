78383feb42b68ad9362d3d2fea331d1795351309
diyanovamir <amirdianov@gmail.com>
2023-05-10 14:29:16 +0300

    + websockets send msg

--- backend/comments_system/consumers.py
+++ backend/comments_system/consumers.py
@@ -1,7 +1,19 @@
+import json
+
 from channels.generic.websocket import WebsocketConsumer
 
 
 class MainConsumer(WebsocketConsumer):
     def connect(self):
-        print("Connect!")
+        print("connect!", self.channel_name)
+        self.channel_layer.group_add("main", self.channel_name)
         self.accept()
+
+    def receive(self, text_data=None, bytes_data=None):
+        if text_data is None:
+            return
+
+        data = json.loads(text_data)
+        print(data)
+        # self.channel_layer.group_send("main", data)
+        self.send(text_data)
--- frontend/event-loop/services/notifications.js
+++ frontend/event-loop/services/notifications.js
@@ -4,6 +4,14 @@ export function initNotifications() {
     const ws = new WebSocket(`${WEBSOCKET_URL}/server/`);
     ws.onclose = console.log;
     ws.onerror = console.log;
-    ws.onopen = console.log;
     ws.onmessage = console.log;
+
+    ws.onopen = () => {
+        console.log('open');
+        setTimeout(() => {
+            ws.send(JSON.stringify({
+                'message': 'hello world'
+            }));
+        }, 1000);
+    };
 }
