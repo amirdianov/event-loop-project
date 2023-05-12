0ec978e00b4db34e092c7485f9307050ad9c88f8
diyanovamir <amirdianov@gmail.com>
2023-05-13 00:46:59 +0300

    + comments from db

--- backend/comments_system/serializers.py
+++ backend/comments_system/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
-from comments_system.models import Comment
+from authorisation_token.serializers import UserProfileSerializer
+from comments_system.models import Comment, User
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source="user.name", read_only=True)
+
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ("created_at", "text", "user_name")
--- backend/comments_system/views.py
+++ backend/comments_system/views.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from comments_system.models import Comment
@@ -13,3 +14,11 @@ class CommentView(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet
 
     def get_queryset(self, *args, **kwargs):
         return Comment.objects.filter(event_id=kwargs["event_id"])
+
+    def list(self, request, *args, **kwargs):
+        print(request.GET["event_id"])
+        queryset = self.filter_queryset(
+            self.get_queryset(event_id=request.GET["event_id"])
+        )
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
