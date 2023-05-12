7bed554b9e48e72a77f306dab3dd2b1db5170a9a
diyanovamir <amirdianov@gmail.com>
2023-05-12 23:41:51 +0300

    + comments view, serializer

--- backend/comments_system/urls.py
+++ backend/comments_system/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from authorisation_token.views import (
     status_view,
 )
+from comments_system.views import CommentView
 
 urlpatterns = [
     path("", status_view, name="status"),
+    path(
+        "event_comments/",
+        CommentView.as_view({"get": "list", "post": "create"}),
+        name="comments",
+    ),
 ]
--- backend/comments_system/views.py
+++ backend/comments_system/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+from comments_system.models import Comment
+from comments_system.serializers import CommentSerializer
+
 
 # Create your views here.
+
+
+class CommentView(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        return Comment.objects.filter(event_id=kwargs["event_id"])
