c498757d59c1e9331abc2c0600e57a66d1454bb9
diyanovamir <amirdianov@gmail.com>
2023-05-18 14:10:47 +0300

    + icons with success and denied pay

--- frontend/event-loop/src/views/general/PayResponseView.vue
+++ frontend/event-loop/src/views/general/PayResponseView.vue
@@ -1,19 +1,34 @@
 <template>
-    <h1>{{ this.text }}</h1>
+    <a-row type="flex" justify="center" align="middle" style="min-height: 550px">
+        <a-col>
+            <LoadingOutlined v-if="isLoading" :style="{fontSize: '50px', color: '#4d2a16'}"/>
+            <div v-if="!isLoading">
+                <img v-if="text === 'Успешно!'" src="@/assets/images/top-up-credit-concept-illustration_114360-7244.png"
+                     alt="Успешный платеж" width="500" height="500">
+                <img v-else src="@/assets/images/business-people-saying-no-concept-illustration_114360-14201.png"
+                     alt="Неуспешный платеж" width="500" height="500">
+            </div>
+        </a-col>
+    </a-row>
 </template>
 
 <script>
 import {getPayResponse, subscribe} from "../../../services/api";
+import {LoadingOutlined} from "@ant-design/icons-vue";
+import LoginComponent from "@/containers/user/LoginComponent.vue";
 
 export default {
     name: "PayResponseView",
+    components: {LoginComponent, LoadingOutlined},
     data() {
         return {
-            text: ''
+            text: '',
+            isLoading: false
         }
     },
     methods: {
         async getResponse() {
+            this.isLoading = true
             try {
                 const payInfo = await getPayResponse(this.$route.query.payment_id);
                 console.log(payInfo);
@@ -24,7 +39,9 @@ export default {
                 } else {
                     this.text = 'Ошибка транзакции';
                 }
+                this.isLoading = false
             } catch (error) {
+                this.isLoading = false
                 console.error(error);
             }
         }
